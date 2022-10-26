24fd890ddc43c581ceb19eb712fa37df2e7431cc
Heika <heikakakao@gmail.com>
2022-10-26 15:48:21 +0700

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
